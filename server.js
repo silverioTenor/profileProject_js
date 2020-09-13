e09ae561293a3f124101e3ebec112d6e6d76f2db
silverioTenor <silverio_tenor@outook.com>
2020-09-12 21:47:46 -0300

    Separação dos dados entre o front e o back-end. Redução do código na área de links.

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
-const express = require('express');
-const nunjucks = require('nunjucks');
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const server = express();
-const videos = require("./data");
+const server = express()
+const videos = require("./data")
 
-server.use(express.static('public'));
+server.use(express.static('public'))
 
-server.set("view engine", "njk");
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express: server
 })
 
-server.get("/", (req, res) => res.render("about"));
+server.get("/", (req, res) => {
+    const data = {
+        name: "Rodrigo Silvério",
+        job: "Desenvolvedor Full-Stack",
+        photo: "luffy.jpg",
+        description: "Focado nas melhores tecnologias para o desenvolvimento de aplicações web e mobile, afim de trazer a melhor experiência para o usuário."
+    }
 
-server.get("/portfolio", (req, res) => res.render("portfolio", {items: videos}));
+    const social = [
+        {
+            name: "GitHub",
+            url: "https://github.com/silverioTenor/"
+        },
+        {
+            name: "Twitter",
+            url: "https://twitter.com/silverio_tenor"
+        },
+        {
+            name: "linkedIn",
+            url: "https://www.linkedin.com/in/rodrigo-silverio-659502139/"
+        }
+    ]
 
-server.listen(3000, () => console.log("Server is running..."));
+    return res.render("about", { data, links: social })
+});
+
+server.get("/portfolio", (req, res) => res.render("portfolio", { items: videos }))
+
+server.listen(3000, () => console.log("Server is running..."))
